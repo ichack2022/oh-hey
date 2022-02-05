b54b546607fa9bebdb5d546a40d9efa6a3c4fac6
Pranav <pb719@ic.ac.uk>
2022-02-05 22:57:31 +0000

    ADD OAuth stuff

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,74 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import webex from './webex';
+import * as vscode from "vscode";
+
+import startServer from "./webex";
+
+const Webex = require(`webex`);
+const webex = Webex.init({
+  credentials: {
+    access_token:
+      "NWYyYmE4YTgtODkxZi00NDhkLWE1ZDEtM2VhNGQzZWMzZDYzOWRiYTFlNTEtM2Ux_PF84_3f179706-f11a-4ab7-ba3e-57a4a96b089f",
+  },
+});
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-messages" is now active!');
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "vscode-messages" is now active!'
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let codeMessage = vscode.commands.registerCommand('vscode-messages.sendCodeMessage', () => {
-		vscode.window.showInformationMessage('Typescript is good');
-	});
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let codeMessage = vscode.commands.registerCommand(
+    "vscode-messages.webexLogin",
+    () => {
+      vscode.window.showInformationMessage("Typescript is good");
+      vscode.env.openExternal(
+        vscode.Uri.parse(
+          "https://webexapis.com/v1/authorize?client_id=C8609770741382a6add794423a26f06154db2ae0f6e256b6c33a8a873d7ddc0c1&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8000&scope=spark-admin%3Aworkspaces_write%20spark%3Acalls_write%20spark-admin%3Aresource_groups_read%20spark%3Aall%20spark%3Akms%20Identity%3Aone_time_password%20spark-admin%3Aroles_read%20identity%3Atokens_write%20identity%3Atokens_read&state=set_state_here"
+        )
+      );
+      // console.log(res);
+      startServer();
 
-	let regularMessage = vscode.commands.registerCommand('vscode-messages.sendRegularMessage', () => {
-		vscode.window.showInformationMessage('Typescript is bad');
-	});
-	console.log("added");
+      // console.log(res);
 
-	context.subscriptions.push(codeMessage);
-	context.subscriptions.push(regularMessage);
+      // auth().then(() => {
+      //   // Create a room with the title "My First Room"
+      //   // Add Alice and Bob to the room
+      //   // Send a **Hi Everyone** message to the room
+      //   webex.rooms.create({ title: `My Second Room` }).then((room: any) => {
+      //     return Promise.all([
+      //       webex.memberships.create({
+      //         roomId: room.id,
+      //         personEmail: `pranavbansal19@imperial.ac.uk`,
+      //       }),
+      //     ]).then(() =>
+      //       webex.messages.create({
+      //         markdown: `**Hi Everyone**`,
+      //         roomId: room.id,
+      //       })
+      //     );
+      //   });
+      // });
+    }
+  );
 
+  let regularMessage = vscode.commands.registerCommand(
+    "vscode-messages.sendRegularMessage",
+    () => {
+      vscode.window.showInformationMessage("Typescript is bad");
+    }
+  );
+  console.log("added");
+
+  context.subscriptions.push(codeMessage);
+  context.subscriptions.push(regularMessage);
 }
 
 // this method is called when your extension is deactivated
--- src/webex.ts
+++ src/webex.ts
@@ -1,34 +1,76 @@
-import fetch from "node-fetch";
+//
+// Copyright (c) 2016 Cisco Systems
+// Licensed under the MIT License
+//
+/*
+ * a Webex Teams Integration based on Node.js, that acts on user's behalf.
+ * implements the Webex OAuth flow, to retreive an API access tokens.
+ *
+ * See documentation: https://developer.webex.com/authentication.html
+ *
+ */
 
-const Webex = require(`webex`);
-const webex = Webex.init({
-  credentials: {
-    access_token:
-      "NWYyYmE4YTgtODkxZi00NDhkLWE1ZDEtM2VhNGQzZWMzZDYzOWRiYTFlNTEtM2Ux_PF84_3f179706-f11a-4ab7-ba3e-57a4a96b089f",
-  },
-});
+// Load environment variables from project .env file
+// require("node-env-file")(__dirname + "/.env");
 
-async function auth() {
-  const res = await fetch(
-    "https://webexapis.com/v1/authorize?client_id=C8609770741382a6add794423a26f06154db2ae0f6e256b6c33a8a873d7ddc0c1&response_type=code&redirect_uri=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DdQw4w9WgXcQ&scope=spark-admin%3Aworkspaces_write%20spark%3Acalls_write%20spark-admin%3Aresource_groups_read%20spark%3Aall%20spark%3Akms%20Identity%3Aone_time_password%20spark-admin%3Aroles_read%20identity%3Atokens_write%20identity%3Atokens_read&state=set_state_here"
-  );
+const debug = require("debug")("oauth");
+const fine = require("debug")("oauth:fine");
+
+const request = require("request");
+const express = require("express");
+const app = express();
+
+let access_token: string | undefined = undefined;
+
+export function getAccessToken(): string | undefined {
+  return access_token
 }
 
-export default auth().then(() => {
-  // Create a room with the title "My First Room"
-  // Add Alice and Bob to the room
-  // Send a **Hi Everyone** message to the room
-  webex.rooms.create({ title: `My Second Room` }).then((room) => {
-    return Promise.all([
-      webex.memberships.create({
-        roomId: room.id,
-        personEmail: `pranavbansal19@imperial.ac.uk`,
-      }),
-    ]).then(() =>
-      webex.messages.create({
-        markdown: `**Hi Everyone**`,
-        roomId: room.id,
-      })
-    );
-  });
+// const initiateURL =
+// "https://webexapis.com/v1/authorize?client_id=C8609770741382a6add794423a26f06154db2ae0f6e256b6c33a8a873d7ddc0c1&response_type=code&redirect_uri=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DdQw4w9WgXcQ&scope=spark-admin%3Aworkspaces_write%20spark%3Acalls_write%20spark-admin%3Aresource_groups_read%20spark%3Aall%20spark%3Akms%20Identity%3Aone_time_password%20spark-admin%3Aroles_read%20identity%3Atokens_write%20identity%3Atokens_read&state=set_state_here";
+// const join = require("path").join;
+
+// const read = require("fs").readFileSync;
+// const str = read(join(__dirname, "/www/index.ejs"), "utf8");
+// const ejs = require("ejs");
+
+
+
+app.get("/", (req: any, res: any) => {
+  const options = {
+    method: "POST",
+    url: "https://api.ciscospark.com/v1/access_token",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+    },
+    form: {
+      grant_type: "authorization_code",
+      client_id:
+        "C8609770741382a6add794423a26f06154db2ae0f6e256b6c33a8a873d7ddc0c1",
+      client_secret:
+        "728b1bfae38f8ded8cc6160bc777f53f1dbf57b85be2fb592942ade5f403166a",
+      code: req.query.code,
+      redirect_uri: "http://localhost:8000",
+    },
+  };
+
+  request(options, (error: any, response: any, body: any) => {
+    console.log("HERE1");
+    const json = JSON.parse(body);
+    access_token = json.access_token
+   // console.log(json.access_token);
+    // console.log(json);
+    res.send({
+      "code": req.query.code,
+      "token": access_token
+    })
+  }
 });
+
+function startServer(): void {
+  app.listen(8000, function () {
+    console.log("Webex OAuth Integration started on port: " + 8000);
+  });
+}
+
+export default startServer;
