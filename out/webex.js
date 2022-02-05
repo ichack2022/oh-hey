6aea141c8936d317a185708516312f29e2c85ee6
Pranav <pb719@ic.ac.uk>
2022-02-05 17:43:40 +0000

    INPROGRESS

---
+++ out/webex.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const node_fetch_1 = require("node-fetch");
+const Webex = require(`webex`);
+const webex = Webex.init({
+    credentials: {
+        access_token: "NWYyYmE4YTgtODkxZi00NDhkLWE1ZDEtM2VhNGQzZWMzZDYzOWRiYTFlNTEtM2Ux_PF84_3f179706-f11a-4ab7-ba3e-57a4a96b089f",
+    },
+});
+async function auth() {
+    const res = await (0, node_fetch_1.default)("https://webexapis.com/v1/authorize?client_id=C8609770741382a6add794423a26f06154db2ae0f6e256b6c33a8a873d7ddc0c1&response_type=code&redirect_uri=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DdQw4w9WgXcQ&scope=spark-admin%3Aworkspaces_write%20spark%3Acalls_write%20spark-admin%3Aresource_groups_read%20spark%3Aall%20spark%3Akms%20Identity%3Aone_time_password%20spark-admin%3Aroles_read%20identity%3Atokens_write%20identity%3Atokens_read&state=set_state_here");
+}
+exports.default = auth().then(() => {
+    // Create a room with the title "My First Room"
+    // Add Alice and Bob to the room
+    // Send a **Hi Everyone** message to the room
+    webex.rooms.create({ title: `My Second Room` }).then((room) => {
+        return Promise.all([
+            webex.memberships.create({
+                roomId: room.id,
+                personEmail: `pranavbansal19@imperial.ac.uk`,
+            }),
+        ]).then(() => webex.messages.create({
+            markdown: `**Hi Everyone**`,
+            roomId: room.id,
+        }));
+    });
+});
+//# sourceMappingURL=webex.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import webex from './webex';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,13 +14,18 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-messages.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-messages!');
+	let codeMessage = vscode.commands.registerCommand('vscode-messages.sendCodeMessage', () => {
+		vscode.window.showInformationMessage('Typescript is good');
 	});
 
-	context.subscriptions.push(disposable);
+	let regularMessage = vscode.commands.registerCommand('vscode-messages.sendRegularMessage', () => {
+		vscode.window.showInformationMessage('Typescript is bad');
+	});
+	console.log("added");
+
+	context.subscriptions.push(codeMessage);
+	context.subscriptions.push(regularMessage);
+
 }
 
 // this method is called when your extension is deactivated
