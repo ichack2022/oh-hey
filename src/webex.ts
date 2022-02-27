f49256418cbadc5221131ec9c588fa9cecb82768
Pranav <pb719@ic.ac.uk>
2022-02-27 14:55:53 +0000

    UPDATE getAccessToken to use dev-messages-server

--- src/webex.ts
+++ src/webex.ts
@@ -44,35 +44,20 @@ export function getWebex(storageManager: LocalStorageService) {
 function getAccessToken(storageManager: LocalStorageService): string {
   let accessToken: string = storageManager.getValue("access_token");
 
-  const authorizationURL =
-    "https://webexapis.com/v1/authorize?client_id=C8609770741382a6add794423a26f06154db2ae0f6e256b6c33a8a873d7ddc0c1&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8000&scope=spark-admin%3Aworkspaces_write%20spark%3Acalls_write%20spark-admin%3Aresource_groups_read%20spark%3Aall%20spark%3Akms%20Identity%3Aone_time_password%20spark-admin%3Aroles_read%20identity%3Atokens_write%20identity%3Atokens_read&state=set_state_here";
-  const authenticationURL = "https://api.ciscospark.com/v1/access_token";
-
   if (accessToken === null) {
-    vscode.env.openExternal(vscode.Uri.parse(authorizationURL));
+    vscode.env.openExternal(
+      vscode.Uri.parse("https://dev-messages-server.herokuapp.com/webex")
+    );
     app.get("/", async (req: any, res: any) => {
-      const options = {
-        method: "POST",
-        url: authenticationURL,
-        headers: {
-          "content-type": "application/x-www-form-urlencoded",
-        },
-        form: {
-          grant_type: "authorization_code",
-          client_id:
-            "C8609770741382a6add794423a26f06154db2ae0f6e256b6c33a8a873d7ddc0c1",
-          client_secret:
-            "728b1bfae38f8ded8cc6160bc777f53f1dbf57b85be2fb592942ade5f403166a",
-          code: req.query.code,
-          redirect_uri: "http://localhost:8000",
-        },
-      };
-      request(options, (error: any, response: any, body: any) => {
-        const json = JSON.parse(body);
-        accessToken = json.access_token;
-        storageManager.setValue("access_token", accessToken);
+      const encodedJSON = req.query.json;
 
-        res.send(`
+      const decodedJSON = Buffer.from(encodedJSON, "base64").toString();
+      console.log("JSON", decodedJSON);
+
+      const json = JSON.parse(decodedJSON);
+      accessToken = json.access_token;
+      storageManager.setValue("access_token", accessToken);
+      res.send(`
         <h1>Success! Feel free to close this :)</h1>
         <script>
           console.log("Closing in 3 seconds");
@@ -81,10 +66,9 @@ function getAccessToken(storageManager: LocalStorageService): string {
           }, 3000);
         </script>
         `);
-        // app.close();
-      });
     });
-    app.listen(8000, function () {
+
+    app.listen(3000, function () {
       console.log("Webex OAuth Integration started on port: " + 8000);
     });
   }
